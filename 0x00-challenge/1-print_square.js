7a7fc0c0324f0c72236c860c41aae9dbcb6cfe1b
Benjamin Wachira <wachirabenjamin18@gmail.com>
2023-05-28 09:39:19 +0300

    Print square

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -16,12 +16,9 @@ if (process.argv.length <= 2) {
 
 size = parseInt(process.argv[2])
 
-var i
-var j
-
-for (for i = 0 ; i < size ; i ++)
+for (let i = 0 ; i < size ; i ++)
 {
-	for (j = 0; j < size; j++)
+	for (let j = 0; j < size; j++)
 	{
 		process.stdout.write('#');
 	}
